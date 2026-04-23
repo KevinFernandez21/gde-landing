c133a2938de06765036826d22786f568c96ed74a
KevinFernandez21 <kdfernan@espol.edu.ec>
2026-04-23 08:38:56 -0500

    resolve problem vercel

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Hero from '@/components/Hero'
 const Stats          = dynamic(() => import('@/components/Stats'),          { ssr: false })
 const BrandsBar      = dynamic(() => import('@/components/BrandsBar'),      { ssr: false })
 const AboutUs        = dynamic(() => import('@/components/AboutUs'),        { ssr: false })
-const GlobalPresence = dynamic(() => import('@/components/GlobalPresence'), { ssr: false })
 const Services       = dynamic(() => import('@/components/Services'),       { ssr: false })
 const TechStack      = dynamic(() => import('@/components/TechStack'),      { ssr: false })
 const HowItWorks     = dynamic(() => import('@/components/HowItWorks'),     { ssr: false })
